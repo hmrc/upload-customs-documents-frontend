a86a267ccda0ab4fdc2581fbe4d96f61143b56ab
Artur Opala <250927+arturopala@users.noreply.github.com>
2022-11-17 11:51:22 +0000

    revert bootstrap-frontend-play-28 upgrade

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -3,7 +3,7 @@ import sbt._
 object AppDependencies {
 
   val compile = Seq(
-    "uk.gov.hmrc"                  %% "bootstrap-frontend-play-28" % "7.10.0",
+    "uk.gov.hmrc"                  %% "bootstrap-frontend-play-28" % "7.3.0",
     "uk.gov.hmrc.mongo"            %% "hmrc-mongo-play-28"         % "0.74.0",
     "uk.gov.hmrc"                  %% "crypto"                     % "7.3.0",
     "uk.gov.hmrc"                  %% "play-frontend-hmrc"         % "3.33.0-play-28",
