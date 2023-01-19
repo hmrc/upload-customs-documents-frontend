dd2863e5689f776a8b3a33fbfc059592ff561250
Artur Opala <250927+arturopala@users.noreply.github.com>
2023-01-19 14:49:30 +0000

    upgrade dependencies and clean build warnings

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -5,21 +5,20 @@ object AppDependencies {
   val compile = Seq(
     "uk.gov.hmrc"                  %% "bootstrap-frontend-play-28" % "7.12.0",
     "uk.gov.hmrc.mongo"            %% "hmrc-mongo-play-28"         % "0.74.0",
-    "uk.gov.hmrc"                  %% "crypto"                     % "7.3.0",
-    "uk.gov.hmrc"                  %% "play-frontend-hmrc"         % "3.34.0-play-28",
-    "com.fasterxml.jackson.module" %% "jackson-module-scala"       % "2.12.5",
+    "uk.gov.hmrc"                  %% "play-frontend-hmrc"         % "6.2.0-play-28",
+    "com.fasterxml.jackson.module" %% "jackson-module-scala"       % "2.14.1",
     "com.sun.mail"                  % "javax.mail"                 % "1.6.2",
     "org.jsoup"                     % "jsoup"                      % "1.15.3"
   )
 
   val test = Seq(
-    "org.scalamock"          %% "scalamock"          % "5.2.0"    % Test,
-    "org.scalatest"          %% "scalatest"          % "3.2.14"   % "test, it",
+    "org.scalamock"          %% "scalamock"          % "5.2.0"    % "test",
+    "org.scalatest"          %% "scalatest"          % "3.2.15"   % "test, it",
     "com.vladsch.flexmark"    % "flexmark-all"       % "0.64.0"   % "test, it",
     "org.scalameta"          %% "munit"              % "0.7.29"   % "test, it",
     "org.scalacheck"         %% "scalacheck"         % "1.17.0"   % "test, it",
     "org.scalatestplus"      %% "scalacheck-1-15"    % "3.2.11.0" % "test, it",
     "org.scalatestplus.play" %% "scalatestplus-play" % "5.1.0"    % "test, it",
-    "com.github.tomakehurst"  % "wiremock-jre8"      % "2.27.2"   % "it"
+    "com.github.tomakehurst"  % "wiremock-jre8"      % "2.35.0"   % "it"
   )
 }
