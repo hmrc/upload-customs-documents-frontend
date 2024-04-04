6c954141e166931f76845069cf39862db576092f
Artur Opala <250927+arturopala@users.noreply.github.com>
2024-04-04 09:08:02 +0100

    update nodejs dependencies to fix compilation issues

--- app/assets/javascripts/setup/init.ts
+++ app/assets/javascripts/setup/init.ts
@@ -1,4 +1,4 @@
-import GOVUKFrontend from 'webjars/lib/govuk-frontend/govuk/all.js';
+import GOVUKFrontend from 'webjars/lib/govuk-frontend/dist/govuk/all.bundle.js';
 import HMRCFrontend from 'webjars/lib/hmrc-frontend/hmrc/all.js';
 
 export default function
