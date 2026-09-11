133e6569de788f5c6b9040f23dff47de71fbc550
Sergi <97970044+sergi-hmrc@users.noreply.github.com>
2026-09-11 16:46:16 +0100

    DCWL-4717: update dependencies

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -7,8 +7,8 @@ object AppDependencies {
 
   val compile: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"                  %% s"bootstrap-frontend$playSuffix"  % bootstrapVersion,
-    "uk.gov.hmrc.mongo"            %% "hmrc-mongo-play-30"              % "2.12.0",
-    "uk.gov.hmrc"                  %% "play-frontend-hmrc-play-30"      % "13.9.0",
+    "uk.gov.hmrc.mongo"            %% "hmrc-mongo-play-30"              % "2.13.0",
+    "uk.gov.hmrc"                  %% "play-frontend-hmrc-play-30"      % "13.13.0",
     "com.sun.mail"                  % "jakarta.mail"                    % "2.0.2",
     "org.jsoup"                     % "jsoup"                           % "1.22.2",
     "com.fasterxml.jackson.module" %% "jackson-module-scala"            % "2.22.1",
